9dd23a95bc45bc961140e20d418e26f5e5c0827c
lumusitech <lumusitech@gmail.com>
2021-12-07 09:34:53 -0300

    [ADD] simple routerLinks added into main/menu and main/statistics module

--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StatisticsRoutingModule } from './statistics-routing.module';
+import { StatisticsMenuComponent } from './pages/statistics-menu/statistics-menu.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StatisticsMenuComponent
+  ],
   imports: [
     CommonModule,
     StatisticsRoutingModule
