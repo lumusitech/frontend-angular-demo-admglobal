63b39979c6e01e241c4b909cb472c79a25046843
lumusitech <lumusitech@gmail.com>
2021-12-09 16:45:55 -0300

    [FIX] rename folder printers/menu to printers/components

--- src/app/printers/printers.module.ts
+++ src/app/printers/printers.module.ts
@@ -6,7 +6,7 @@ import { PrinterComponent } from './pages/printer/printer.component';
 import { PrintersListComponent } from './pages/printers-list/printers-list.component';
 import { NewPrinterComponent } from './pages/new-printer/new-printer.component';
 import { PrintersDashboardComponent } from './pages/printers-dashboard/printers-dashboard.component';
-import { PrintersMenuComponent } from './menu/printers-menu/printers-menu.component';
+import { PrintersMenuComponent } from './components/printers-menu/printers-menu.component';
 
 
 @NgModule({
