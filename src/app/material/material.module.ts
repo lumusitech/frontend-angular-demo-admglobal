2f8354d8db4c714c76d0a25b83ce4d043274d635
lumusitech <lumusitech@gmail.com>
2021-12-15 19:31:12 -0300

    [EDIT] changed users list with material - first steps

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 const myModules: any = [
+  FlexLayoutModule,
   MatButtonModule,
+  MatFormFieldModule,
   MatIconModule,
+  MatInputModule,
   MatListModule,
+  MatPaginatorModule,
   MatSidenavModule,
-  MatToolbarModule
+  MatTableModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../material/material.module';
 import { NewUserComponent } from './pages/new-user/new-user.component';
 import { NewUserTaskComponent } from './pages/new-user-task/new-user-task.component';
 import { UserComponent } from './pages/user/user.component';
+import { UsersDashboardComponent } from './pages/users-dashboard/users-dashboard.component';
 import { UsersListComponent } from './pages/users-list/users-list.component';
+import { UsersMenuComponent } from './components/users-menu/users-menu.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { UserTaskComponent } from './pages/user-task/user-task.component';
 import { UserTasksListComponent } from './pages/user-tasks-list/user-tasks-list.component';
-import { UsersDashboardComponent } from './pages/users-dashboard/users-dashboard.component';
-import { UsersMenuComponent } from './components/users-menu/users-menu.component';
 
 
 @NgModule({
@@ -17,15 +18,16 @@ import { UsersMenuComponent } from './components/users-menu/users-menu.component
     NewUserComponent,
     NewUserTaskComponent,
     UserComponent,
+    UsersDashboardComponent,
     UsersListComponent,
+    UsersMenuComponent,
     UserTaskComponent,
     UserTaskComponent,
     UserTasksListComponent,
-    UsersDashboardComponent,
-    UsersMenuComponent,
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     UsersRoutingModule
   ]
 })
